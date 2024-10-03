b0d9a4d6e0c1c4029f1a450232aee932c8130bb9
Shravan_124 <shravankumardharam124@gmail.com>
2024-10-03 22:14:30 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');  // Updated from jade to pug
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 // Error handler middleware
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
@@ -30,7 +31,6 @@ app.use(function(err, req, res, next) {
     error: process.env.NODE_ENV === 'development' ? err : {} // Hide error details in production
   });
 });
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
