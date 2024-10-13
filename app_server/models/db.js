5274062bb22a56f086db078c4db59a6697497a60
Shravan_124 <shravankumardharam124@gmail.com>
2024-10-13 23:19:26 +0530

    .

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,41 +1,48 @@
 // models/db.js
 const mongoose = require('mongoose');
-const dbURI = "mongodb://localhost:27017/roast";
+require('dotenv').config(); // Load environment variables
+const dbURI = process.env.DB_URI || "mongodb://localhost:27017/roast"; // Fallback to default
 
-mongoose.connect(dbURI);
+mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected to ${dbURI}`);
+    console.log(`Mongoose connected to ${dbURI}`);
 });
+
 mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err);
+    console.log('Mongoose connection error:', err);
 });
+
 mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose disconnected');
+    console.log('Mongoose disconnected');
 });
 
 // Require the Review model
 require('./review');
 
-var gracefulShutdown = function (msg, callback) {
-  mongoose.connection.close(function () {
-    console.log('Mongoose disconnected through ' + msg);
-    callback();
-  });
+const gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
 };
 
 // Handle process termination signals
 process.once('SIGUSR2', function () {
-  gracefulShutdown('nodemon restart', function () {
-    process.kill(process.pid, 'SIGUSR2');
-  });
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
 });
 process.on('SIGINT', function () {
-  gracefulShutdown('app termination', function () {
-    process.exit(0);
-  });
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
 });
 process.on('SIGTERM', function() {
-  gracefulShutdown('Heroku app shutdown', function () {
-    process.exit(0);
-  });
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
 });
